920527d08d103417151e94a0122975ac4433cb7a
Augusto fragg <augustofragg@gmail.com>
2026-04-27 22:24:59 -0300

    Saving data to the database using @Repository

---
+++ src/main/java/tech/buildrun/demojpa/controller/UserController.java
@@ -0,0 +1,34 @@
+package tech.buildrun.demojpa.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.buildrun.demojpa.controller.dto.CreateUserDto;
+import tech.buildrun.demojpa.entity.UserEntity;
+import tech.buildrun.demojpa.service.UserService;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserDto dto) {
+
+
+        UserEntity user = userService.createUser(dto);
+
+        return ResponseEntity.created(URI.create("/users/" + user.getUserId())).build();
+
+    }
+}
---
+++ src/main/java/tech/buildrun/demojpa/service/UserService.java
@@ -0,0 +1,28 @@
+package tech.buildrun.demojpa.service;
+
+import org.springframework.stereotype.Service;
+import tech.buildrun.demojpa.controller.dto.CreateUserDto;
+import tech.buildrun.demojpa.entity.UserEntity;
+import tech.buildrun.demojpa.repository.UserRepository;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserEntity createUser(CreateUserDto dto) {
+
+        UserEntity user = new UserEntity();
+        user.setName(dto.name());
+        user.setAge(dto.age());
+        user.setCreatedAt(LocalDateTime.now());
+
+        return userRepository.save(user);
+    }
+}
